4b79346c1d9053fba3f44a6cc3db680b7a9ee48c
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2024-10-08 00:24:53 +0800

    implement addItem

--- cart.go
+++ cart.go
@@ -1,6 +1,6 @@
 package main
 
-import "fmt"
+var cart = Cart{}
 
 type CartItem struct {
 	ProductID int
@@ -10,10 +10,27 @@ type Cart struct {
 	CartItems []CartItem
 }
 
-func (c *CartItem) setCartItem(productID, quantity int) {
-	c.ProductID = productID
-	c.Quantity = quantity
+func (c *Cart) findProductInCart(productID int) int {
+	for i, item := range c.CartItems {
+		if productID == item.ProductID {
+			return i
+		}
+	}
+	return -1
 }
-func (c CartItem) getCartItem() string {
-	return fmt.Sprintf("%d,%d", c.ProductID, c.Quantity)
+
+func (c *Cart) addItem(productID, quantity int) {
+	// check if the product is already in cart
+	// if not, add a new cartitem
+	// if yes, add quantity
+	index := c.findProductInCart(productID)
+	if index >= 0 {
+		c.CartItems[index].Quantity += quantity
+		return
+	}
+	item := CartItem{
+		ProductID: productID,
+		Quantity:  quantity,
+	}
+	c.CartItems = append(c.CartItems, item)
 }
