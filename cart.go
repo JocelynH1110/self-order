e62aa55ce721256b201a053168abb749e028356e
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2024-10-25 17:46:12 +0800

    WIP on sorting cart items in the database

--- cart.go
+++ cart.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"database/sql"
+)
+
 var cart = Cart{}
 
 type CartItem struct {
@@ -11,17 +15,26 @@ type Cart struct {
 }
 
 // findProductInCart 對輸入的商品跟購物車內商品是否相同
-func (c *Cart) findProductInCart(productID int) int {
+/*func (c *Cart) findProductInCart(productID int) int {
 	for i, item := range c.CartItems {
 		if productID == item.ProductID {
 			return i
 		}
 	}
 	return -1
+}*/
+
+func (c *Cart) findProductInCart(db *sql.DB, productID int) (*CartItem, error) {
+	row := db.QueryRow("select id, quantity,product_id from cart_items where product_id = ?", productID)
+	var i CartItem
+	if err := row.Scan(&i.ProductID, &i.Quantity); err != nil {
+		return nil, err
+	}
+	return &i, nil
 }
 
 // addItem 新增商品到購物車
-func (c *Cart) addItem(productID, quantity int) {
+/*func (c *Cart) addItem(productID, quantity int) {
 	// check if the product is already in cart
 	// if not, add a new cartitem
 	// if yes, add quantity
@@ -35,12 +48,30 @@ func (c *Cart) addItem(productID, quantity int) {
 		Quantity:  quantity,
 	}
 	c.CartItems = append(c.CartItems, item)
+}*/
+
+func (c *Cart) addItem(db *sql.DB, productID, quantity int) (*CartItem, error) {
+	row := db.QueryRow("insert into cart_items(id, quantity,product_id) values(?,?)")
+	var i CartItem
+	if err := row.Scan(&i.ProductID, &i.Quantity); err != nil {
+		return nil, err
+	}
+	return &i, nil
 }
 
 // removeItem 刪除購物車內商品
-func (c *Cart) removeItem(productID int) {
+/*func (c *Cart) removeItem(productID int) {
 	index := c.findProductInCart(productID)
 	if index >= 0 {
 		c.CartItems = append(c.CartItems[:index], c.CartItems[index+1:]...)
 	}
+}*/
+func (c *Cart) removeItem(db *sql.DB, productID int) (*CartItem, error) {
+	row := db.QueryRow("delete from cart_items where id = ?", productID)
+
+	var i CartItem
+	if err := row.Scan(&i.ProductID, &i.Quantity); err != nil {
+		return nil, err
+	}
+	return &i, nil
 }
--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,7 @@
-select 42;
+pragma foreign_keys = ON;
+-- please also run this line after connecting to the db in the program
+-- https://www.sqlite.org/foreignkeys.html
+
 create table products(
   id integer primary key,
   name varchar(20) not null,
@@ -6,4 +9,15 @@ create table products(
   description varchar(50)
 );
 
-insert into products (name,price,description) values ('Filter coffee',80,null),('Latte',120,'coffee con leche'),('Oolong tea',100,null),('Cylen black tea',100,'tea from cylen'),('Milk tea',150,'te con leche');
+insert into products (name,price,description) values ('Filter coffee',80,null),('Latte',120,'Coffee con leche'),('Oolong tea',100,null),('Ceylon tea',100,'Tea from Sri Lanka'),('Milk tea',150,'Te con leche');
+
+create table cart_items(
+  id integer primary key,
+	quantity  integer not null ,
+	product_id integer ,
+  inserted_at integer , -- store as Unix timestamp
+  updated_at integer ,
+  FOREIGN KEY(product_id) REFERENCES products(id) ON DELETE CASCADE
+);
+
+insert into cart_items(quantity,inserted_at) values (?,strftime('%s','now'));
