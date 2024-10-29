efc882eb5faf9e10fee9da0560f9d5977ae04f14
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2024-10-29 22:06:11 +0800

    WIP on cart sql integration

--- db/init.sql
+++ db/init.sql
@@ -13,11 +13,12 @@ insert into products (name,price,description) values ('Filter coffee',80,null),(
 
 create table cart_items(
   id integer primary key,
-	quantity  integer not null ,
-	product_id integer ,
-  inserted_at integer , -- store as Unix timestamp
-  updated_at integer ,
-  FOREIGN KEY(product_id) REFERENCES products(id) ON DELETE CASCADE
+	quantity  integer not null default 1,
+	product_id integer not null unique,
+  inserted_at integer not null default (strftime('%s','now')), -- store as Unix timestamp
+  updated_at integer not null default (strftime('%s','now')),
+  FOREIGN KEY(product_id) REFERENCES products(id) ON DELETE CASCADE,
+  check (quantity>0)
 );
 
-insert into cart_items(quantity,inserted_at) values (?,strftime('%s','now'));
+insert into cart_items(quantity,product_id) values (1,1);
