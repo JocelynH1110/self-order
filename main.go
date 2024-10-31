4ac56edc231ed83c3c2d6815f82069e13e349e03
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2024-11-01 00:49:06 +0800

    serve menu over HTTP

--- main.go
+++ main.go
@@ -1,21 +1,17 @@
 package main
 
 import (
-	"bufio"
 	"database/sql"
 	"fmt"
 	"log"
-	"os"
-	"strconv"
-
-	"strings"
+	"net/http"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 const RESTAURANT_NAME string = "Jo's coffee shop!"
 
-func parseRemoveCommand(tokens []string) (ok bool, productID int) {
+/*func parseRemoveCommand(tokens []string) (ok bool, productID int) {
 	if len(tokens) != 2 {
 		return
 	}
@@ -42,56 +38,71 @@ func parseAddCommand(tokens []string) (ok bool, productID int, quantity int) {
 	}
 	return true, int(id), int(qty)
 }
+*/
 
-// 現在 cart 依賴資料庫連接，不能存在全域變數內，故可在 handleCommand 內初始化
-func handleCommand(db *sql.DB, command string) {
-	cart := Cart{db}
-	tokens := strings.Split(command, " ")
-	switch tokens[0] {
-	case "menu":
-		fmt.Println("Menu：")
+func menuHandler(db *sql.DB) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		lists, err := listProducts(db)
 		if err != nil {
 			log.Fatal(err)
 		}
+		fmt.Fprintf(w, `<title>%s</title><h1>%s</h1><h2>Menu</h2><ol>`, RESTAURANT_NAME, RESTAURANT_NAME)
 		for _, product := range lists {
-			fmt.Printf("%d. %s, $%d\n", product.ID, product.Name, product.Price)
-		}
-	case "remove":
-		if ok, productID := parseRemoveCommand(tokens); ok {
-			cart.removeItem(productID)
-			return
-		}
-		fmt.Println("USAGE: add PRODUCT_ID [QUANTITY]")
-	case "add":
-		if ok, productID, quantity := parseAddCommand(tokens); ok {
-			cart.addItem(productID, quantity)
-			return
-		}
-		fmt.Println("USAGE: add PRODUCT_ID [QUANTITY]")
-
-	case "cart":
-		items, err := cart.listProductsInCart()
-		if err != nil {
-			log.Fatal(err)
-		}
-		if items == nil {
-			fmt.Println("Your cart is currently empty.")
-			return
-		}
-		fmt.Printf("You have %v item(s) in the shopping cart.\n", len(items))
-		for index, item := range items {
-			fmt.Printf("%d. %s, %d x $%d = $%d\n", index+1, item.Name, item.Quantity, item.Price, item.Subtotal)
+			fmt.Fprintf(w, "<li>%s, $%d</li>\n", product.Name, product.Price)
 		}
 
-	case "quit":
-		os.Exit(0)
-
-	default:
-		fmt.Println("Unrecognized command. Command should be one of: menu, cart, add, quit.")
-	}
+	})
 }
 
+/*
+	func handleCommand(db *sql.DB, command string) {
+		cart := Cart{db} // 現在 cart 依賴資料庫連接，不能存在全域變數內，故可在 handleCommand 內初始化
+		tokens := strings.Split(command, " ")
+		switch tokens[0] {
+		case "menu":
+			fmt.Println("Menu：")
+			lists, err := listProducts(db)
+			if err != nil {
+				log.Fatal(err)
+			}
+			for _, product := range lists {
+				fmt.Printf("%d. %s, $%d\n", product.ID, product.Name, product.Price)
+			}
+		case "remove":
+			if ok, productID := parseRemoveCommand(tokens); ok {
+				cart.removeItem(productID)
+				return
+			}
+			fmt.Println("USAGE: add PRODUCT_ID [QUANTITY]")
+		case "add":
+			if ok, productID, quantity := parseAddCommand(tokens); ok {
+				cart.addItem(productID, quantity)
+				return
+			}
+			fmt.Println("USAGE: add PRODUCT_ID [QUANTITY]")
+
+		case "cart":
+			items, err := cart.listProductsInCart()
+			if err != nil {
+				log.Fatal(err)
+			}
+			if items == nil {
+				fmt.Println("Your cart is currently empty.")
+				return
+			}
+			fmt.Printf("You have %v item(s) in the shopping cart.\n", len(items))
+			for index, item := range items {
+				fmt.Printf("%d. %s, %d x $%d = $%d\n", index+1, item.Name, item.Quantity, item.Price, item.Subtotal)
+			}
+
+		case "quit":
+			os.Exit(0)
+
+		default:
+			fmt.Println("Unrecognized command. Command should be one of: menu, cart, add, quit.")
+		}
+	}
+*/
 func main() {
 	db, err := sql.Open("sqlite3", "db/dev.db")
 	if err != nil {
@@ -99,7 +110,10 @@ func main() {
 	}
 	defer db.Close()
 
-	fmt.Printf("Welcome to %s\n", RESTAURANT_NAME)
+	http.Handle("/", menuHandler(db))
+	log.Fatal(http.ListenAndServe(":3000", nil))
+
+	/*fmt.Printf("Welcome to %s\n", RESTAURANT_NAME)
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("> ")
@@ -110,5 +124,5 @@ func main() {
 		}
 		line := scanner.Text()
 		handleCommand(db, line)
-	}
+	}*/
 }
